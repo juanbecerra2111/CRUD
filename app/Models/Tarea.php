d0d6dbced31b049960a8dec7225869ca3bd71a20
Samuel Mercado <samuel.mg@gmx.com>
2025-10-07 16:55:41 -0600

    Crea TareaFactory
    - Crea TareaFactory con comando `php artisan make:factory -m Tarea TareaFactory`
    - Nota: Opcionalmente al crear un modelo se puede utilizar `php artisan make:model -f MiModelo`
    - Configura el modelo Tarea para utilizar factory con `use HasFactory;`

--- app/Models/Tarea.php
+++ app/Models/Tarea.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tarea extends Model
 {
+    use HasFactory;
+
     public $timestamps = false;
 }
