3adb3902b559354170fde2526a9d4d8d187721b7
Samuel Mercado <samuel.mg@gmx.com>
2025-10-14 16:13:07 -0600

    Implementamos layout app (default de laravel) en index-tareas

--- resources/views/tareas/index-tareas.blade.php
+++ resources/views/tareas/index-tareas.blade.php
@@ -1,47 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tareas</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-<body>
-    <h1>Listado de Tareas</h1>
-    <ul>
-        <li>
-            <a href="{{ route('tarea.create') }}">Crear nueva tarea</a>
-        </li>
-    </ul>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Título</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($tareas as $tarea)
-                <tr>
-                    <td>{{ $tarea->id }}</td>
-                    <td>
-                        <a href="{{ route('tarea.show', $tarea->id) }}">
-                            {{ $tarea->titulo }}
-                        </a>
-                    </td>
-                    <td>
-                        <a class="btn btn-warning" href="{{ route('tarea.edit', $tarea->id) }}">Editar</a>
-                        <form action="{{ route('tarea.destroy', $tarea->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger" type="submit">Eliminar</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</body>
-</html>
+<x-layouts.app :title="__('Dashboard')">
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+            <h1>Listado de Tareas</h1>
+            <ul>
+                <li>
+                    <a href="{{ route('tarea.create') }}">Crear nueva tarea</a>
+                </li>
+            </ul>
+            <div class="overflow-x-auto">
+            <table class="min-w-full divide-y-2 divide-gray-200">
+                <thead class="ltr:text-left rtl:text-right">
+                    <tr class="*:font-medium *:text-gray-900">
+                        <th class="px-3 py-2 whitespace-nowrap">ID</th>
+                        <th class="px-3 py-2 whitespace-nowrap">Título</th>
+                        <th class="px-3 py-2 whitespace-nowrap">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tareas as $tarea)
+                        <tr>
+                            <td>{{ $tarea->id }}</td>
+                            <td>
+                                <a href="{{ route('tarea.show', $tarea->id) }}">
+                                    {{ $tarea->titulo }}
+                                </a>
+                            </td>
+                            <td>
+                                <a class="btn btn-warning" href="{{ route('tarea.edit', $tarea->id) }}">Editar</a>
+                                <form action="{{ route('tarea.destroy', $tarea->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="submit">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            </div>
+        </div>
+    </div>
+</x-layouts.app>
