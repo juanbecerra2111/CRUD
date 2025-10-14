836693f488237f240412614e1ea8433bcf791eba
Samuel Mercado <samuel.mg@gmx.com>
2025-10-14 15:47:02 -0600

    Crea componente de blade form-error
    Crea componente (solo vista): `php artisan make:component form-error --view`

---
+++ resources/views/components/form-error.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <h3>Aqui se muestran los errores (con componente)</h3>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+</div>
