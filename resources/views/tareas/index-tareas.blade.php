68455d58e32db8fe06167ddef2507a9a11b19e98
Samuel Mercado <samuel.mg@gmx.com>
2025-10-14 15:48:13 -0600

    Agrega CDN de bootstrap

--- resources/views/tareas/index-tareas.blade.php
+++ resources/views/tareas/index-tareas.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tareas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
     <h1>Listado de Tareas</h1>
@@ -13,7 +14,7 @@
             <a href="{{ route('tarea.create') }}">Crear nueva tarea</a>
         </li>
     </ul>
-    <table border="1">
+    <table class="table">
         <thead>
             <tr>
                 <th>ID</th>
@@ -31,15 +32,16 @@
                         </a>
                     </td>
                     <td>
-                        <a href="{{ route('tarea.edit', $tarea->id) }}">Editar</a>
+                        <a class="btn btn-warning" href="{{ route('tarea.edit', $tarea->id) }}">Editar</a>
                         <form action="{{ route('tarea.destroy', $tarea->id) }}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <button type="submit">Eliminar</button>
+                            <button class="btn btn-danger" type="submit">Eliminar</button>
                         </form>
                     </td>
                 </tr>
             @endforeach
         </tbody>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
