0c43b0732c7be795f4bfe1a2c5387e538f0f376b
Samuel Mercado <samuel.mg@gmx.com>
2025-10-14 16:49:29 -0600

    Implementa mi-layout en vista create-tarea

--- resources/views/tareas/create-tarea.blade.php
+++ resources/views/tareas/create-tarea.blade.php
@@ -1,29 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tarea: Crear</title>
-</head>
-<body>
+<x-mi-layout>
     <h1>Creación de nueva tarea</h1>
 
     @include('form-error')
 
     <form action="{{ route('tarea.store') }}" method="POST">
         @csrf
-        <label for="titulo">Título:</label>
-        <input
-            type="text"
-            id="titulo"
-            name="titulo"
-            value="{{ old('titulo') }}"
-            >
-        @error('titulo')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-        <br>
+        <div class="mb-3">
+            <label class="form-label" for="titulo">Título:</label>
+            <input
+                class="form-control"
+                type="text"
+                id="titulo"
+                name="titulo"
+                value="{{ old('titulo') }}"
+                >
+            @error('titulo')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+
         <label for="descripcion">Descripción:</label>
         <textarea id="descripcion" name="descripcion">{{ old('descripcion') }}</textarea>
         @error('descripcion')
@@ -32,5 +28,4 @@
         <br>
         <button type="submit">Guardar</button>
     </form>
-</body>
-</html>
+</x-mi-layout>
