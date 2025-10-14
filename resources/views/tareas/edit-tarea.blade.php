f611803ce27e0a9dfd911034a9c259e96dcb5465
Samuel Mercado <samuel.mg@gmx.com>
2025-10-14 15:48:00 -0600

    Implementación de componentes de blade

--- resources/views/tareas/edit-tarea.blade.php
+++ resources/views/tareas/edit-tarea.blade.php
@@ -5,10 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tarea: Editar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Editar tarea</h1>
-    @include('form-error')
+    <x-encabezado>
+        Editar Tarea: {{ $tarea->titulo }}
+    </x-encabezado>
+
+    {{-- @include('form-error') --}}
+    <x-form-error />
     <form action="{{ route('tarea.update', $tarea->id) }}" method="POST">
         @csrf
         @method('PATCH')
