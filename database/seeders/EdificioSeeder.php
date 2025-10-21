c74bfe49529d79c1e2bcdc70819f866e0774246d
Samuel Mercado <samuel.mg@gmx.com>
2025-10-21 16:40:37 -0600

    Implementación de seeder en tabla edificios
    - Crea modelo, migración, seeder y factory para Edificio
    - Implementa EdificioSeeder
    - Implementa EdificioFactory
    - Llama EdificioSeeder desde DatabasSeeder

---
+++ database/seeders/EdificioSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Edificio;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EdificioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Inserta información utilizando Query Builder
+        DB::table('edificios')->insert([
+            ['nombre' => 'Edificio A', 'niveles' => 5],
+            ['nombre' => 'Edificio B', 'niveles' => 10],
+            ['nombre' => 'Edificio C', 'niveles' => 3],
+        ]);
+
+        // Inserta información utilizando factories
+        Edificio::factory()->count(10)->create();
+
+        // Inserta información utilizando el modelo
+        Edificio::create([
+            'nombre' => 'Edificio Especial',
+            'niveles' => 15,
+        ]);
+    }
+}
